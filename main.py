c2abd61c7f70a65b1194608b048ec763b82bfcde
Paul Delgado <paulrdelgado@gmail.com>
2022-01-24 13:35:52 -0500

    created jupyter notebook

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import tensorflow as tf
 
+def model_creation():
+	model = tf.keras.Sequential()
+	model.add(tf.keras.layers.Conv1D(40, 10, activation='relu'))
+	return model
+
 def main():
-	pass
+	model = model_creation()
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 if __name__ == "__main__":
 	print("running main.py script")
